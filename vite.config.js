6d33d4f13584f304174af4db52b25aae148ef5d5
LithilKimar <lithilkumars@gmail.com>
2025-11-07 07:35:52 +0530

    modified code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // 👈 allows access from Docker host
+    port: 5173,        // same port you exposed in Dockerfile
+    strictPort: true,  // ensures it won’t auto-switch ports
+    watch: {
+      usePolling: true, // 👈 useful for Docker file change detection
+    },
+  },
 })
